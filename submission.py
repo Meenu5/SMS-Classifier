a2e55be47570d2b982a86aa8a026300929011171
MEENAKSHI RAVISANKAR <ravisankarmeenakshi@gmail.com>
2018-01-29 23:04:21 +0530

    Update submission.py

--- submission.py
+++ submission.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import xgboost as xgb
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -12,8 +11,10 @@ from collections import defaultdict
 import operator
 
 df = pd.read_csv('../TRAIN_SMS.csv', encoding='latin-1')
+
 df = df.rename(columns={"Label":"label", "Message":"sms_text"})
 messages = pd.Series(df['sms_text'].tolist())
+
 ham_messages = pd.Series(df[df['label'] == 'ham']['sms_text'].tolist())
 spam_messages = pd.Series(df[df['label'] == 'spam']['sms_text'].tolist())
 info_messages = pd.Series(df[df['label'] == 'info']['sms_text'].tolist())
@@ -25,15 +26,19 @@ dist_info = info_messages.apply(len)
 
 df['word_count'] = pd.Series(df['sms_text'].tolist()).apply(lambda x: len(x.split(' ')))
 df['char_count'] = pd.Series(df['sms_text'].tolist()).apply(len)
+
 df['label'][df['label']=='ham']=0
 df['label'][df['label']=='info']=1
 df['label'][df['label']=='spam']=2
+
 X = df[['word_count', 'char_count']]
 y = df[['label']]
 y=pd.to_numeric(y['label'],errors='coerce')
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
+# param check
 params = {}
 params['objective'] = 'multi:softmax'
 params['eval_metric'] = 'merror'
@@ -43,17 +48,18 @@ params['num_class'] = 3
 d_train = xgb.DMatrix(X_train, label=y_train)
 d_valid = xgb.DMatrix(X_valid, label=y_valid)
 
-# param check
 
 watchlist = [(d_train, 'train'), (d_valid, 'valid')]
 bst = xgb.train(params, d_train, 400, watchlist, early_stopping_rounds=50, verbose_eval=10)
+
 # Predict values for test set
 d_test = xgb.DMatrix(X_test)
 p_test = bst.predict(d_test)
-# Apply function round() to each element in np array
-# so predictions are all either 0 or 1.
+
+# predictions - 0 or 1
 npround = np.vectorize(round)
 p_test_ints = npround(p_test)
+
 # Error rate for test set
 accuracy = accuracy_score(y_test, p_test_ints)
 print("Test Accuracy: ", accuracy)
@@ -62,30 +68,27 @@ X = df
 y = df[['label']]
 y=pd.to_numeric(y['label'],errors='coerce')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 ham_messages_train = pd.Series(X_train[X_train['label'] == 0]['sms_text'].tolist()).astype(str)
 spam_messages_train = pd.Series(X_train[X_train['label'] == 2]['sms_text'].tolist()).astype(str)
 info_messages_train = pd.Series(X_train[X_train['label'] == 1]['sms_text'].tolist()).astype(str)
 
-# WordCloud automatically excludes stop words
 spam_messages_one_string = " ".join(spam_messages_train.astype(str))
 ham_messages_one_string = " ".join(ham_messages_train.astype(str))
 info_messages_one_string = " ".join(info_messages_train.astype(str))
 
 ham_words_list = ham_messages_one_string.split()
 total_ham_words = len(ham_words_list)
-print("Total number of words in ham messages: ", total_ham_words)
 ham_words_dict = Counter(ham_words_list).most_common()
 ham_words_dict[:25]
 
 spam_words_list = spam_messages_one_string.split()
 total_spam_words = len(spam_words_list)
-print("Total number of words in spam messages: ", total_spam_words)
 spam_words_dict = Counter(spam_words_list).most_common()
 spam_words_dict[:25]
 
 info_words_list = info_messages_one_string.split()
 total_info_words = len(info_words_list)
-print("Total number of words in info messages: ", total_info_words)
 info_words_dict = Counter(info_words_list).most_common()
 info_words_dict[:25]
 stopwords = set(stopwords.words("english"))
@@ -133,9 +136,11 @@ for word in info_words_top25_list :
 for word in ["due in","flt","booking id","repair ref","reservation"] :
     if word not in uniquemap :
         uniquemap[word]=1
+        
 dff = df['sms_text'].str.lower()
 Xx_train = X_train['sms_text'].str.lower()
 Xx_test  = X_test['sms_text'].str.lower()
+
 for word in uniquemap.keys() :
     df[word] =  list(int(y==True) for y in list(word in x for x in dff))
     X_train[word] =  list(int(y==True) for y in list(word in x for x in Xx_train))
@@ -150,8 +155,10 @@ X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_siz
 X_train = X_train[list(X_train.columns)]
 X_test = X_test[list(X_test.columns)]
 X_valid = X_valid[list(X_train.columns)]
+
 d_train = xgb.DMatrix(X_train, label=y_train)
 d_valid = xgb.DMatrix(X_valid, label=y_valid)
+
 watchlist = [(d_train, 'train'), (d_valid, 'valid')]
 bst = xgb.train(params, d_train, 400, watchlist, early_stopping_rounds=50, verbose_eval=10)
 d_test = xgb.DMatrix(X_test)
@@ -160,12 +167,16 @@ npround = np.vectorize(round)
 p_test_ints = npround(p_test)
 accuracy = accuracy_score(y_test, p_test_ints)
 print("Test Accuracy: ", accuracy)
+
 #### Submission file ####
 df = pd.read_csv('../DEV_SMS.csv', encoding='latin-1')
+
 df = df.rename(columns={"RecordNo":"RecordNo", "Message":"sms_text"})
 RecordNo =np.array(df["RecordNo"]).astype(int)
+
 df['word_count'] = pd.Series(df['sms_text'].tolist()).apply(lambda x: len(x.split(' ')))
 df['char_count'] = pd.Series(df['sms_text'].tolist()).apply(len)
+
 X = df
 Xx  = X['sms_text'].str.lower()
 for word in uniquemap.keys() :
